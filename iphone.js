d028d26a8caacd0f16dd0b8a77be2d046fad8a99
jtreeves <jackson.t.reeves@gmail.com>
2020-10-21 15:36:48 -0400

    [js] Created objects for Star Wars and iPhone

---
+++ iphone.js
@@ -0,0 +1,10 @@
+const iphone12 = {
+    image: 'path to where image is located',
+    length: '11:28',
+    title: 'iPhone 12 Unboxing Experience',
+    username: 'Marques Brownlee',
+    views: '5.5M',
+    uploaded: 15
+};
+
+console.log(iphone12.title);
---
+++ star-wars.js
@@ -0,0 +1,10 @@
+const starwars = {
+    title: 'Special Look | The Mandalorian | Disney+',
+    username: 'Star Wars',
+    image: 'path to where image is located',
+    length: '1:00',
+    views: '1M',
+    uploaded: '1 day ago'
+};
+
+console.log(starwars.title);
